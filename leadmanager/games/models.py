2b01b3924fc447cfb5a72e6ade09e33fd0f25a59
Ha Van <vannunganha@gmail.com>
2019-06-03 15:37:08 +0300

    Change database to postgresql

--- leadmanager/games/models.py
+++ leadmanager/games/models.py
@@ -3,12 +3,56 @@ from django.db import models
 # Create your models here.
 
 
+class GameCategory(models.Model):
+    name = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ('name',)
+    # The method will be called whenever it has to provide a human-readable representation for the model
+
+    def __str__(self):
+        return self.name
+
+
 class Game(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     name = models.CharField(max_length=200, blank=True, default='')
     released_dated = models.DateTimeField()
-    game_category = models.CharField(max_length=200, default='', blank=True)
+    # Many-to-one relationship with the game category model
+    # CASCADE: Whenever a game category is deleted, all the games that belong to this category will be deleted too
+    game_category = models.ForeignKey(
+        GameCategory, related_name='games', on_delete=models.CASCADE)
     played = models.BooleanField(default=False)
 
     class Meta:
         ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class Player(models.Model):
+    MALE = 'M'
+    FEMALE = 'F'
+    GENDER_CHOICES = ((MALE, 'Male'), (FEMALE, 'Female'),)
+    created = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=200, blank=True, default='')
+    gender = models.CharField(
+        max_length=2, choices=GENDER_CHOICES, default=MALE)
+
+    class Meta:
+        ordering = ('name',)
+
+    def __str__(self):
+        return self.name
+
+
+class PlayScore(models.Model):
+    player = models.ForeignKey(
+        Player, related_name="scores", on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    score = models.IntegerField()
+    score_date = models.DateTimeField()
+
+    class Meta:
+        ordering = ('-score',)
--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -93,8 +93,12 @@ WSGI_APPLICATION = 'leadmanager.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'games',
+        'USER': 'test1',
+        'PASSWORD': 'TestingPassword',
+        'HOST': '127.0.0.1',
+        'PORT': '5432'
     }
 }
 
